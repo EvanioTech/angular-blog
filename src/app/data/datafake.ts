b2523f11f3fd11c606fa414d326d9b40f9ea431f
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2023-12-18 16:20:31 -0300

    add description news

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -2,8 +2,8 @@ export const dataFake = [
 {
   "id" :"1",
   "photo":"aaa",
-  "title": "saiu novo filme",
-  "description": "ola",
+  "title": "IA nossa nova aliada!",
+  "description": "A IA pode ser usada para automatizar tarefas rotineiras e repetitivas, permitindo que os seres humanos se concentrem em atividades mais criativas e estratégicas. Isso aumenta a eficiência e reduz o tempo dedicado a tarefas monótonas.",
   "photoCover": "https://www.telesintese.com.br/wp-content/uploads/2023/04/Inteligencia-Artificial-IA-AI-freepik-2023.jpg"
 
 },
@@ -11,7 +11,7 @@ export const dataFake = [
   "id":'2',
   "photo":"aaa",
   "title": "IOT a famosa internet das coisas!",
-  "description": "ola",
+  "description": "A Internet das Coisas (IoT) descreve a rede de objetos físicos incorporados a sensores, software e outras tecnologias com o objetivo de conectar e trocar dados com outros dispositivos e sistemas pela internet. Esses dispositivos variam de objetos domésticos comuns a ferramentas industriais sofisticadas. Com mais de 7 bilhões de dispositivos IoT conectados hoje, os especialistas esperam que esse número cresça para 10 bilhões em 2020 e 22 bilhões em 2025. A Oracle tem uma rede de parceiros de dispositivo.",
   "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxvCV-pChRjbaWiLYaktsrP3lgnkRAE3cG2A&usqp=CAU"
 
 },
@@ -19,7 +19,7 @@ export const dataFake = [
   "id" :'3',
   "photo":"aaa",
   "title": "Machine lerning",
-  "description": "ola",
+  "description": "Machine learning é uma área da inteligência artificial (IA) e da ciência da computação que se concentra no uso de dados e algoritmos para imitar a maneira como os humanos aprendem, melhorando gradualmente sua precisão.",
   "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7rosG8cI1Nn07-HLUO9Ytp4KMunOwPCmvHw&usqp=CAU"
 
 },
@@ -27,7 +27,7 @@ export const dataFake = [
   "id":'4',
   "photo":"aaa",
   "title": "Cultura Maker",
-  "description": "ola",
+  "description": "O que é Cultura Maker? O movimento Cultura Maker é uma evolução do “Do it yourself” ou, em bom português, do “Faça você mesmo”. O conceito principal é que qualquer pessoa, dotada das ferramentas certas e do devido conhecimento, pode criar as suas próprias soluções para problemas do cotidiano.",
   "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHGX06VXBnhghmehhmP8ssc2KEPgCIQDW1ng&usqp=CAU"
 
 }
